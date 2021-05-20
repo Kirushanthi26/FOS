7d685c659dc4dad4b4014cc42fdc943a3a6a6fc0
kirushanthi <kirushanthikiru26@gmail.com>
2021-05-20 14:34:53 +0530

    create and design the register.php file

--- Login_part/login.php
+++ Login_part/login.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!-- Popper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!-- css style sheet -->
-    <link rel="stylesheet" href="../css/loginStyle.css">
-</head>
+<?php 
+ include "login_header.php";
+?>
 <body>
     <div class="container-fluid">
         <div class="row">
@@ -37,6 +20,8 @@
                         <label for="password">Password</label>
                         <input type="password" name="password" id="password" required>
                         </div>
+
+                        <!-- alert part start-->
                         <div class="inputForm">
                             <?php if(!empty($error_info)){?>
                             <div class="alert alert-danger alert-dismissible">
@@ -45,6 +30,8 @@
                             </div>
                             <?php } ?>
                         </div>
+                        <!-- alert part end -->
+
                         <button name="login" class="btn btn-success">Login</button>
                         <button type="reset" class="btn btn-danger">Cancel</button>
                     </form>
---
+++ Login_part/register.php
@@ -0,0 +1,62 @@
+<?php 
+ include "login_header.php";
+?>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 d-none d-md-block img-fluid image-container">
+            </div>
+
+            <div class="col-md-6 form-container">
+                <div class="col-md-12 form-group form-box text-center">
+                    <h1 id="loghead">BeeChef</h1>
+                    <h3>Registration</h3>
+                    <h6 id="right-p">Create Your Account... Enjoy Fastest Food Service..!</h6>
+                    <form action="register.php" method="POST" enctype="multipart/form-data">
+                    <div class="inputForm">
+                        <label for="name">Name: </label>
+                        <input type="text" name="name" id="name" required>
+                    </div>
+                    <div class="inputForm">
+                    <label for="address">Address: </label>
+                     <input type="text" name="address" id="address" required>
+                    </div>
+                    <div class="inputForm">
+                    <label for="email">Email: </label>
+                    <input type="email" name="email" id="email" required>
+                    </div>
+                    <div class="inputForm">
+                    <label for="phoneNo">Tell: </label>
+                    <input type="tel" name="phoneNo" id="phoneNo" required>
+                    </div>
+                    <div class="inputForm">
+                    <label for="username">Username: </label>
+                    <input type="text" name="username" id="username" required>
+                    </div>
+                    <div class="inputForm">
+                    <label for="password">Password: </label>
+                    <input type="password" name="password" id="password" required>
+                    </div>
+                    <div class="inputForm">
+                    <label for="confirm">Confirm Password: </label>
+                    <input type="password" name="confirm" id="confirm" required>
+                    </div>
+                    <div class="inputForm">
+                    <?php 
+                        // showing the error message
+                        if(!empty($error_info)){ ?>
+                        <div class="alert alert-danger alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                            <?= $error_info ?></div>
+                        <?php } ?>
+                    </div>
+                    <button name="Register" class="btn btn-success">Register</button>
+                    <button type="reset" class="btn btn-danger">Cancel</button>
+                    </form>
+                    <a href="login.php">Already have an account? Login now!!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
