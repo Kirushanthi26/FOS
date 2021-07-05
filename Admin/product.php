588edc6157825ffe1f3ca6e4a7bedcf0f905a878
kirushanthi <kirushanthikiru26@gmail.com>
2021-07-05 14:55:13 +0530

    fetch the product table on the product page

--- Admin/product.php
+++ Admin/product.php
@@ -7,7 +7,11 @@ include "admin_header.php";
 <style>
 <?php include "../css/adminHome.css"; //admin home css part add ?>
 </style>
+<?php
 
+    
+
+?>
 <div class="container-fluid" >
     <div class="row">
         <div class="col-md-2 px-0">
@@ -24,12 +28,19 @@ include "admin_header.php";
         </div>
         <div class="col-md-10">
             <h1 class="page-header text-center page-header-cate">PRODUCTS CRUD</h1>
-            <!-- Button trigger modal -->
-            <div class="text-right">
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addProductModal"><i class="fas fa-plus"></i>Add Product</button>
-            </div>
 
-            <!-- Modal Add category-->
+            <!-- Button trigger modal - add product -->
+            <div class="text-right">
+                <select class="form-control-sm text-left mr-5">
+                    <option>Select Category</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                <button type="button" class="btn btn-primary my-3" data-toggle="modal" data-target="#addProductModal"><i class="fas fa-plus"></i>Add Product</button>
+            </div>
+            <!-- end Button trigger modal - add product -->
+
+            <!-- Modal Add product-->
             <div class="modal fade" id="addProductModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="font-family: 'Poppins', sans-serif;">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -40,7 +51,7 @@ include "admin_header.php";
                             </button>
                         </div>
                         <div class="modal-body">
-                            <form method="POST" action="category.php" enctype="multipart/form-data">
+                            <form method="POST" action="product.php" enctype="multipart/form-data">
                                 <div class="form-group" style="margin-top:10px;">
                                     <div class="row">
                                         <div class="col-md-4" style="margin-top:7px;">
@@ -55,7 +66,7 @@ include "admin_header.php";
                                             <label class="control-label font-weight-bold">Category Name:</label>
                                         </div>
                                         <div class="col-md-8">
-                                            <select class="form-control"name="catName" >
+                                            <select class="form-control-sm"name="catName" >
                                             <option selected>Choose...</option>
                                             <?php
                                                 $sql="SELECT c_name FROM category";
@@ -72,7 +83,7 @@ include "admin_header.php";
                                             <label class="control-label font-weight-bold">Price:</label>
                                         </div>
                                         <div class="col-md-8">
-                                            <input type="text" class="form-control" name="cateName" required>
+                                            <input type="text" class="form-control" name="price" required>
                                         </div>
                                     </div>
                                     <div class="row py-2">
@@ -80,7 +91,7 @@ include "admin_header.php";
                                             <label class="control-label font-weight-bold">Photo:</label>
                                         </div>
                                         <div class="col-md-8">
-                                            <input type="file" class="form-control-file" name="" required>
+                                            <input type="file" class="form-control-file" name="photo" required>
                                         </div>
                                     </div>
                                     <div class="row py-2">
@@ -88,7 +99,7 @@ include "admin_header.php";
                                             <label class="control-label font-weight-bold">Description:</label>
                                         </div>
                                         <div class="col-md-8">
-                                            <textarea class="form-control" rows="2" name="" required></textarea>
+                                            <textarea class="form-control" rows="2" name="desc" required></textarea>
                                         </div>
                                     </div>
                             </div>
@@ -101,8 +112,48 @@ include "admin_header.php";
                     </div>
                 </div>
             </div>
-            <!-- Modal end - Add category -->
+            <!-- Modal end - Add product -->
 
+            <!-- start product table -->
+
+            <table class="table table-hover text-center table-bordered" >
+            <thead>
+                <tr>
+                    <th scope="col">Photo</th>
+                    <th scope="col">Item name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <?php
+                        $sql="SELECT * FROM product ORDER BY pid ASC";
+                        $query=$conn->query($sql);
+					    while($row=$query->fetch_array()){
+                    ?>
+                    <tr>
+                        <td>
+                            <a href="<?php if(empty($row['photo'])){echo "upload/noimage.jpg";} else{echo $row['photo'];} ?>">
+                            <img src="<?php if(empty($row['photo'])){echo "upload/noimage.jpg";} else{echo $row['photo'];} ?>" height="30px" width="40px">
+                            </a>
+                        </td>
+                        <td><b><?php echo $row['p_name']; ?></b>
+                        <?php $idCE=$row['pid']; ?>
+                        </td>
+                        <td>Rs. <?php echo $row['price']; ?></td>
+                        <td><?php echo $row['description']; ?></td>
+                        <td>
+                            <button name="edit" value="<?php echo $row['cid']; ?>" data-toggle="modal" data-target="#editCateModal<?php echo $row['cid']; ?>"class="btn btn-success"><i class="fas fa-edit"></i></button>
+                            <button name="delete" value="<?php echo $row['cid']; ?>" data-toggle="modal" data-target="#deleteCateModal<?php echo $row['cid']; ?>" class="btn btn-danger" ><i class="fas fa-trash-alt"></i></button>
+                            <?php include "categoryModal.php" ?>
+                        </td>
+                    </tr>
+                    <?PHP } ?>
+                </tbody>
+            </table>
+
+            <!-- end product table -->
 
         </div>
     </div>
